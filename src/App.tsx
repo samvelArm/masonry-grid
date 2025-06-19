da225037c4d0c67d602d24b9147e28c7a575a628
Samvel <samvel.antanyan@gmail.com>
2025-06-19 14:05:40 +0400

    added dynamic pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import { Route, Routes } from 'react-router';
-import Grid from './pages/Grid';
-import DetailedView from './pages/DetailedView';
-import Header from './components/Header';
+import { Suspense, lazy } from 'react';
 import styled from 'styled-components';
+import Header from './components/Header';
+import { LoadingSpinner } from './components/LoadingSpinner';
+
+const Grid = lazy(() => import('./pages/Grid'));
+const DetailedView = lazy(() => import('./pages/DetailedView'));
 
 function App() {
   return (
     <Wrapper>
       <Header />
-      <Routes>
-        <Route path="/" element={<Grid />} />
-        <Route path="/:id" element={<DetailedView />} />
-      </Routes>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Routes>
+          <Route path="/" element={<Grid />} />
+          <Route path="/:id" element={<DetailedView />} />
+        </Routes>
+      </Suspense>
     </Wrapper>
   );
 }
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: #f0f0f0;
-`;
 
 export default App;
+
+const Wrapper = styled.div`
+  text-align: center;
+  min-height: 100vh;
+  background-color: #282c34;
+`;
